cb0cc85a60967954202c14572cc2f96032ec6435
Joshua Gladwin <joshuajgladwin@gmail.com>
2022-02-23 15:16:14 +0100

    Initial Commit; Chapter 1;

---
+++ Chapter 1/french_deck.py
@@ -0,0 +1,27 @@
+# Example 1-1. A deck as a sequence of playing cards
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suit = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suit for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
---
+++ README.md
@@ -0,0 +1,10 @@
+# Fluent Python
+
+A workthrough of the code examples from [Fluent Python, by Luciano Ramalho (2. Edition, O’Reilly, 2022)](https://learning.oreilly.com/library/view/fluent-python-2nd/9781492056348/).
+
+This edition of Fluent Python uses Python 3.10.
+
+| Part                | Chapter                               | Contents                                                                         |
+|---------------------|---------------------------------------|----------------------------------------------------------------------------------|
+| I. Prologue         | [1. The Python Data Model](Chapter 1) | [french_deck.py](Chapter 1/french_deck.py), [vector2d.py](Chapter 1/vector2d.py) |
+
